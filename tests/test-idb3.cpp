f928f2e00963a4f45f85e5e8732ae4ecbfff6346
itsme <itsme@xs4all.nl>
2020-11-27 17:26:47 +0100

    updated to use unittestframework.h

--- tests/test-idb3.cpp
+++ tests/test-idb3.cpp
@@ -1,5 +1,4 @@
-#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
-#include "contrib/catch.hpp"
+#include "unittestframework.h"
 
 #include <climits>
 #include "idb3.h"
