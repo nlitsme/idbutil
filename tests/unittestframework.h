0625900a3c2d3510c057d972fb35bb7176e8e93c
itsme <itsme@xs4all.nl>
2022-11-30 13:03:21 +0100

    improved code to locate test framework

--- tests/unittestframework.h
+++ tests/unittestframework.h
@@ -10,33 +10,51 @@
  * using doctest.
  *
  */
-#if !defined(WITH_CATCH) && !defined(WITH_DOCTEST)
-#define WITH_DOCTEST
+#if !defined(USE_CATCH) && !defined(USE_DOCTEST)
+#define USE_DOCTEST
 #endif
 
-#ifdef WITH_CATCH
+#ifdef USE_CATCH
 #ifdef UNITTESTMAIN
 #define CATCH_CONFIG_MAIN 
 #endif
+//#define CATCH_CONFIG_ENABLE_TUPLE_STRINGMAKER
 #define CATCH_CONFIG_ENABLE_ALL_STRINGMAKERS
+#if __has_include(<catch2/catch.hpp>)
+#include <catch2/catch.hpp>
+#elif __has_include("single_include/catch.hpp")
+#include "single_include/catch.hpp"
+#elif __has_include("contrib/catch.hpp")
 #include "contrib/catch.hpp"
+#else
+#error  "Could not find catch.hpp"
+#endif
 
 #define SKIPTEST  , "[!hide]"
 
 // doctest has suites, catch doesn't.
 #define TEST_SUITE(x)  namespace
 
-#elif defined(WITH_DOCTEST)
+#elif defined(USE_DOCTEST)
 #ifdef UNITTESTMAIN
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #endif
 #include <ostream>
+#if __has_include(<doctest/doctest.h>)
+#include <doctest/doctest.h>
+#elif __has_include("contrib/doctest.h")
 #include "contrib/doctest.h"
+#elif __has_include("single_include/doctest.h")
+#include "single_include/doctest.h"
+#else
+#error  "Could not find doctest.h"
+#endif
 #define SECTION(...) SUBCASE(__VA_ARGS__)
 #define SKIPTEST  * doctest::skip(true)
 
 #define CHECK_THAT(a, b) 
 #else
-#error define either WITH_CATCH or WITH_DOCTEST
+#error define either USE_CATCH or USE_DOCTEST
 #endif
 
+#define IN_UNITTEST 1
